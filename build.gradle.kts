88a37613da6c198e09c1909c7ba1624fcd7c22b9
Parakhin Nikolay <66258171+SamuraiGalahad@users.noreply.github.com>
2025-03-18 11:20:50 +0300

    Fixed build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val h2_version: String by project
 val kotlin_version: String by project
 val kotlinx_html_version: String by project
@@ -8,6 +10,7 @@ plugins {
     kotlin("jvm") version "2.1.0"
     id("io.ktor.plugin") version "3.0.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
+    kotlin("plugin.jpa") version "2.1.0"
 }
 
 group = "trotech"
@@ -23,6 +26,10 @@ application {
 repositories {
     mavenCentral()
     maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.freeCompilerArgs += listOf("-Xdebug", "-Xno-optimize")
 }
 
 dependencies {
@@ -43,6 +50,7 @@ dependencies {
     implementation("org.postgresql:postgresql:$postgres_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("io.github.flaxoos:ktor-server-kafka:2.1.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("io.ktor:ktor-network-tls")
     implementation("io.ktor:ktor-server-websockets")
     implementation("io.github.flaxoos:ktor-server-rate-limiting:2.1.2")
@@ -59,4 +67,8 @@ dependencies {
     implementation("org.hibernate:hibernate-entitymanager:5.6.15.Final")
     implementation("org.postgresql:postgresql:42.7.1")
     implementation("javax.persistence:javax.persistence-api:2.2")
+    implementation("org.jetbrains.kotlin.plugin.lombok:org.jetbrains.kotlin.plugin.lombok.gradle.plugin:1.9.24")
+    implementation("org.ktorm:ktorm-core:3.6.0")
+    implementation("io.ktor:ktor-client-content-negotiation")
+    implementation("io.ktor:ktor-server-status-pages")
 }
